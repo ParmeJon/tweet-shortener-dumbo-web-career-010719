78dc1a86cfc173a36cedb89ddbc6440e99478211
Jonathan Chan <jonchan816@gmail.com>
2018-12-27 15:31:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+def dictionary
+    { 
+    "hello" =>"hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  
+end
+
+def word_substituter(str)
+ str_arr = str.split(" ").map do |word|
+   if dictionary.keys.include?(word.downcase)
+     word = dictionary[word.downcase]
+   else
+     word
+   end
+ end.join(" ")
+end
+
+def bulk_tweet_shortener(arr)
+  arr.map do |tweet|
+  puts  word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener
+end
